286c4a88a906e2817628d3cdc3cfd043fed8eafa
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-01-11 00:25:06 +0100

    added round scoring and player lockin

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 
 import random
-from typing import Optional, List, Tuple
+from typing import Dict, Optional, List, Set, Tuple, Union, final
 from uuid import uuid4
 from pydantic import BaseModel, Field, validator
 
@@ -12,7 +12,7 @@ class DiceRoll(BaseModel):
     roll1: int
     roll2: int
 
-    @validator('roll1', 'roll2')
+    @validator('roll1', 'roll2', allow_reuse=True)
     def val_from_1_to_6(cls, roll):
         if not 1 <= roll <= 6:
             raise ValueError("roll must be between 1 and 6")
@@ -29,12 +29,97 @@ class DiceRoll(BaseModel):
         )
         return diceroll
 
+    def is_skunk(self) -> bool:
+        return self.roll1 == 1 or self.roll2 == 1
+
+    def is_super_skunk(self) -> bool:
+        return self.roll1 == 1 and self.roll2 == 1
+            
+
 
 
 
 class Player(BaseModel):
     name: str
     id: str =  Field(default_factory=lambda: str(uuid4()))
+
+    def __hash__(self) -> int:
+        return hash(self.id)
+
+
+class PlayerLockInEvent(BaseModel):
+    player: Player
+
+
+class Round(BaseModel):
+    players: Set[Player] = Field(default_factory=set)
+    events: List[Union[DiceRoll, PlayerLockInEvent]] = Field(default_factory=list)
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+    @classmethod
+    def new(cls) -> Round:
+        return Round()
+
+    def add_player(self, player: Player) -> None:
+        self.players.add(player)
+
+    @property
+    def is_over(self) -> bool:
+        return any(roll.is_skunk() for roll in self.rolls)
+
+    def roll(self):
+        roll = DiceRoll.new()
+        self.add_roll(roll)
+
+    def add_roll(self, roll: DiceRoll):
+        if self.is_over:
+            raise TypeError("Round over.  No more rolls can be added.")
+        self.events.append(roll)
+
+    @property
+    def rolls(self) -> List[DiceRoll]:
+        return [e for e in self.events if isinstance(e, DiceRoll)]
+
+    def add_lockin(self, player: Player):
+        if self.is_over:
+            raise TypeError("Round over.  Too late to lock in score.")
+        
+        event = PlayerLockInEvent(player=player)
+        self.events.append(event)
+
+    @property
+    def scores(self) -> Dict[Player, int]:
+        final_scores = {}
+        running_score = 0
+        for event in self.events:
+            if isinstance(event, DiceRoll):
+                if event.is_skunk():
+                    for player in self.players:
+                        if player not in final_scores:
+                            final_scores[player] = 0
+                    break
+                running_score += event.sum()
+            elif isinstance(event, PlayerLockInEvent):
+                final_scores[event.player] = running_score
+            else:
+                raise TypeError(f"Event not Handled: {event}")
+        else:
+            for player in self.players:
+                if player not in final_scores:
+                    final_scores[player] = running_score
+
+            
+        return final_scores
+            
+
+
+
+                
+
+
 
 
 class Game(BaseModel):
---
+++ test_round.py
@@ -0,0 +1,82 @@
+from pytest import raises
+
+from models import DiceRoll, Player, Round
+
+
+def test_round_logs_roll_history():
+    round = Round.new()
+    round.roll()
+    round.roll()
+    round.roll()
+    assert len(round.rolls) == 3
+
+
+def test_round_calculates_score_with_lockins():
+    
+    
+    round = Round.new()
+    players = [
+        Player(name="Nick"),
+        Player(name="Mary"),
+        Player(name="Joe")
+    ]
+    for player in players:
+        round.add_player(player)
+
+    # roll1: 5
+    roll = DiceRoll(roll1=2, roll2=3)
+    round.add_roll(roll)
+    scores = round.scores
+    assert scores[players[0]] == 5
+    assert scores[players[1]] == 5
+    assert scores[players[2]] == 5
+
+    # roll2: 11
+    roll = DiceRoll(roll1=5, roll2=6)
+    round.add_roll(roll)
+    scores = round.scores
+    assert scores[players[0]] == 16
+    assert scores[players[1]] == 16
+    assert scores[players[2]] == 16
+
+    # player2 locks in, scores don't change
+    round.add_lockin(players[1])
+    scores = round.scores
+    assert scores[players[0]] == 16
+    assert scores[players[1]] == 16
+    assert scores[players[2]] == 16
+
+    # roll3: 4
+    roll = DiceRoll(roll1=2, roll2=2)
+    round.add_roll(roll)
+    scores = round.scores
+    assert scores[players[0]] == 20
+    assert scores[players[1]] == 16
+    assert scores[players[2]] == 20
+
+    # player1 locks in
+    round.add_lockin(players[0])
+
+    # roll4: 7
+    roll = DiceRoll(roll1=3, roll2=4)
+    round.add_roll(roll)
+    scores = round.scores
+    assert scores[players[0]] == 20
+    assert scores[players[1]] == 16
+    assert scores[players[2]] == 27
+    assert len(round.rolls) == 4
+
+    # roll5: Skunk
+    roll = DiceRoll(roll1=6, roll2=1)
+    round.add_roll(roll)
+    scores = round.scores
+    assert scores[players[0]] == 20
+    assert scores[players[1]] == 16
+    assert scores[players[2]] == 0
+    assert len(round.rolls) == 5
+
+    # another roll not possible
+    roll = DiceRoll(roll1=6, roll2=2)
+    with raises(Exception):
+        round.add_roll(roll)
+    assert len(round.rolls) == 5
